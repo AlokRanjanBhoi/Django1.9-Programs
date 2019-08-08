2d42dc8c72269fd795283d1a406549f16f9fd96c
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2019-08-08 23:26:24 +0530

    Add files via upload

---
+++ ManyToMany Relationship/mtoapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    student = models.ForeignKey(Student)
+    name = models.CharField(max_length=100)
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
